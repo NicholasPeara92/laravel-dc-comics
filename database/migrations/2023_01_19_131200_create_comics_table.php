4f9b88d47a1a45cce40269bc387c4c951c042bba
NicholasPeara92 <nicholas.peara@gmail.com>
2023-01-19 14:52:29 +0100

    Add comics db

--- database/migrations/2023_01_19_131200_create_comics_table.php
+++ database/migrations/2023_01_19_131200_create_comics_table.php
@@ -17,10 +17,11 @@ return new class extends Migration
             $table->id();
             $table->string('title', 50);
             $table->string('thumb')->nullable();
-            $table->string('series', 20);
+            $table->string('series', 50);
             $table->date('sale_date');
-            $table->string('type', 10);
-            $table->float('price', 4, 2);
+            $table->string('type', 30);
+            $table->string('price', 6);
+            $table->text('description');
             $table->timestamps();
         });
     }
