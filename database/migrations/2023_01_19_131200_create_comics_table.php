372e2c9a88d23c32897049dadf0ad0862ae9c10d
NicholasPeara92 <nicholas.peara@gmail.com>
2023-01-19 14:28:11 +0100

    first commit

---
+++ database/migrations/2023_01_19_131200_create_comics_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 50);
+            $table->string('thumb')->nullable();
+            $table->string('series', 20);
+            $table->date('sale_date');
+            $table->string('type', 10);
+            $table->float('price', 4, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
